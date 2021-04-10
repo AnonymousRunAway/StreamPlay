ba4949cfc2188e13a332c7640372197b2e167ab3
AnonymousRunAway <47522751+AnonymousRunAway@users.noreply.github.com>
2021-04-10 11:29:33 +0530

    Delete ExternalCall.bat

--- ExternalCall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set execPath=C:/Users/%USERNAME%/Downloads/Music
-set execPathSlashed=C:\Users\%USERNAME%\Downloads\Music
-rem change accordingly
-cd /d %execPath%
-for %%f in (*.mp3) do VbsMaker.bat "%%f" "%execPath%" "%execPathSlashed%"
-
-pause
